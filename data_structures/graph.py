56937d504e82a8273d17afe666b9b3af8ca12d33
mimmi <mimrah14@gmail.com>
2021-04-14 21:28:55 +0500

    CSV Files

---
+++ data_structures/graph.py
@@ -0,0 +1,77 @@
+#   [A] -------- [B]
+#    |   \
+#    |     \   
+#    |        \
+#   [C] -------- [D]
+#   
+#      A  B  C  D
+#   A  0  1  1  1
+#   B  1  0  0  0
+#   C  1  0  0  1
+#   D  1  0  1  0
+
+'''nodes = ['A', 'B', 'C', 'D']
+graph2 = [
+    [0, 1, 1, 1],
+    [1, 0, 0, 0],
+    [1, 0, 0, 1],
+    [1, 0, 1, 0]
+]'''
+
+class node:
+    def __init__(self, name, size):
+        self.name = name
+        self.edges = [None] * size
+    def connect(self, node_name):
+        for x in range(len(self.edges)):
+            if self.edges[x] == node_name:
+                break
+            if self.edges[x] == None:
+                self.edges[x] = node_name
+                break
+    def display(self):
+        print(self.name)
+        print(self.edges)
+
+class graph:
+    def __init__(self, size):
+        self.nodes = [None] * size
+        self.size = size
+    def display(self):
+        for x in range(len(self.nodes)):
+            if (self.nodes[x] != None):
+                print(self.nodes[x].name)
+                print(self.nodes[x].edges)
+    def create_node(self, node_name):
+        new_node = node(node_name, self.size - 1)
+        for x in range(len(self.nodes)):
+            if self.nodes[x] == new_node:
+                break
+            if self.nodes[x] == None:
+                self.nodes[x] = new_node
+                break
+    def create_connection(self, origin, destination):
+        for x in range(len(self.nodes)):
+            if self.nodes[x] != None:
+                if self.nodes[x].name == origin:
+                    self.nodes[x].connect(destination)
+                if self.nodes[x].name == destination:
+                    self.nodes[x].connect(origin)
+'''
+G = graph(10)
+G.create_node('A')
+G.create_node('B')
+G.create_node('C')
+G.create_node('D')
+G.create_connection('A', 'B')
+G.create_connection('A', 'C')
+G.create_connection('A', 'D')
+G.create_connection('C', 'D')
+G.display()
+
+#a = node('A')
+#a.connect('B')
+#a.connect('C')
+#a.connect('C')
+#a.display()
+'''
--- test.py
+++ test.py
@@ -1,76 +1,19 @@
-#   [A] -------- [B]
-#    |   \
-#    |     \   
-#    |        \
-#   [C] -------- [D]
-#   
-#      A  B  C  D
-#   A  0  1  1  1
-#   B  1  0  0  0
-#   C  1  0  0  1
-#   D  1  0  1  0
+csv_file = open("data.csv", 'r')
+file_content = csv_file.read()
 
-nodes = ['A', 'B', 'C', 'D']
-graph2 = [
-    [0, 1, 1, 1],
-    [1, 0, 0, 0],
-    [1, 0, 0, 1],
-    [1, 0, 1, 0]
-]
+lines = file_content.split('\n')
 
-class node:
-    def __init__(self, name, size):
-        self.name = name
-        self.edges = [None] * size
-    def connect(self, node_name):
-        for x in range(len(self.edges)):
-            if self.edges[x] == node_name:
-                break
-            if self.edges[x] == None:
-                self.edges[x] = node_name
-                break
-    def display(self):
-        print(self.name)
-        print(self.edges)
+big_array = []
+for line in lines:
+    line_content = line.split(",")
+    big_array.append(line_content)
 
-class graph:
-    def __init__(self, size):
-        self.nodes = [None] * size
-        self.size = size
-    def display(self):
-        for x in range(len(self.nodes)):
-            if (self.nodes[x] != None):
-                print(self.nodes[x].name)
-                print(self.nodes[x].edges)
-    def create_node(self, node_name):
-        new_node = node(node_name, self.size - 1)
-        for x in range(len(self.nodes)):
-            if self.nodes[x] == new_node:
-                break
-            if self.nodes[x] == None:
-                self.nodes[x] = new_node
-                break
-    def create_connection(self, origin, destination):
-        for x in range(len(self.nodes)):
-            if self.nodes[x] != None:
-                if self.nodes[x].name == origin:
-                    self.nodes[x].connect(destination)
-                if self.nodes[x].name == destination:
-                    self.nodes[x].connect(origin)
+print(len(big_array))
 
-G = graph(10)
-G.create_node('A')
-G.create_node('B')
-G.create_node('C')
-G.create_node('D')
-G.create_connection('A', 'B')
-G.create_connection('A', 'C')
-G.create_connection('A', 'D')
-G.create_connection('C', 'D')
-G.display()
+'''
 
-#a = node('A')
-#a.connect('B')
-#a.connect('C')
-#a.connect('C')
-#a.display()
+array = [
+    [ST001, Ahmed, 1990-01-01],
+    [ST002, Ali, 1991-01-01],
+    [ST003, Aishath, 1992-01-01]
+]'''
